dbd13b3035a869521a30cbbe5c185cae8a1f0c72
Joe Alcorn <joealcorn123@gmail.com>
2017-09-21 16:19:43 +0100

    Add path to formatted error

--- graphql/error/format_error.py
+++ graphql/error/format_error.py
@@ -8,4 +8,13 @@ def format_error(error):
             for loc in error.locations
         ]
 
+    if error.nodes:
+        node = error.nodes[0]
+        path = [node]
+        while node.ancestor:
+            node = node.ancestor
+            path.insert(0, node)
+
+        formatted_error['path'] = [(n.alias or n.name).value for n in path]
+
     return formatted_error
